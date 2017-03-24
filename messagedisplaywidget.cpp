e87a6c97f8b9fbe4796cafe6bff6f31ba7d3e73d
Bclio <bixiaoxia1015@126.com>
2017-03-24 11:05:46 +0800

    信息提示框
    信息提示框添加注释.

--- messagedisplaywidget.cpp
+++ messagedisplaywidget.cpp
@@ -1,5 +1,6 @@
 #include "messagedisplaywidget.h"
 #include "ui_messagedisplaywidget.h"
+#include <QEventLoop>
 
 MessageDisplayWidget::MessageDisplayWidget(QWidget *parent) :
     FramelessModalMovableShadowWidget(parent),
@@ -13,7 +14,10 @@ MessageDisplayWidget::~MessageDisplayWidget()
     delete ui;
 }
 
-void MessageDisplayWidget::showMessage(QWidget *parent, const QString &sTitle, const QString &sInfo, MessageDisplayButtonType okBtn, MessageDisplayButtonType btn)
+void MessageDisplayWidget::showMessage(QWidget *parent, const QString &sTitle, const QString &sInfo,
+                                       MessageDisplayButtonType okBtn,
+                                       MessageDisplayButtonType btn)
 {
+    ///Mark,使用QEventLoop.参照InputTextWidget ==》"inputtextwidget.h"实现.
 
 }
--- messagedisplaywidget.h
+++ messagedisplaywidget.h
@@ -1,3 +1,13 @@
+/**
+ * XDCalendar Project 2017-03-18 version
+  * 风格化的信息提示框，替代QMessageBox.
+  * @example MessageDisplayWidget::showMessage;
+  *
+  * @author BiXiaoxia
+  * @version 2017.03.18.
+  * @author Changmanyi
+  * @version 2017.03.24.
+*/
 #ifndef XDSF_MESSAGEDISPLAYWIDGET_H
 #define XDSF_MESSAGEDISPLAYWIDGET_H
 
